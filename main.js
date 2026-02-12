ac8342b0b30424e6308f506c58858dc54e3dc703
Icefer7w7 <miguelangelgarzonabu@gmail.com>
2026-02-12 17:50:46 -0500

    ahora todo se ve en el prompt

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 var RandomNumber = parseInt(Math.random()*10)+1;
 console.log("secret Numer", RandomNumber);
 var num;
+var message = "Coloca your number between 1 and 10";
 
 while(RandomNumber !=num){
-    num = parseInt(prompt("Coloca your number between 1 and 10"));
+    num = parseInt(prompt(message));
 
     var difference=Math.abs(RandomNumber-num);
 
     if(RandomNumber == num){
-        console.log("CORRECT")
+        alert("CORRECT");
     }
     else if(difference <= 3){
-        console.log("HOT")
+        message= "HOT";
     }
     else{
-        console.log("Cold")
+        message= "Cold";
     }
 
 }
