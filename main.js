d00d1ab9cef1a529f0561257d5ad81b5d6dac4be
306-diseno WEB <306@laboratorios.usbbog.edu.co>
2026-02-12 14:13:18 -0500

    Prueba de funcionamiento

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-<q3 o{unrc }
+var RandomNumber = Math.random()*10
+var num;
+
+while(RandomNumber !=num){
+    num = (prompt("Coloca your number"));
+
+    var difference=Math.abs(RandomNumber-num);
+
+    if(RandomNumber == num){
+        console.log("CORRECT")
+    }
+    else{
+        console.log("Cold")
+    }
+
+}
